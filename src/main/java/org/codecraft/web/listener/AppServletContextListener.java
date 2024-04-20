ae4dc6d3a5c77a113e54f558c9578ed2a75c548c
Christophe <christophe.pauliac@gmail.com>
2024-04-20 20:56:50 +0200

    Codecraft flyway versioning

--- src/main/java/org/codecraft/web/listener/AppServletContextListener.java
+++ src/main/java/org/codecraft/web/listener/AppServletContextListener.java
@@ -1,31 +1,42 @@
-package org.test.listener;
+package org.codecraft.web.listener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 
-public class TestServletContextListener implements ServletContextListener {
+public class AppServletContextListener implements ServletContextListener {
+
+  private static final String URL = "jdbc:postgresql://localhost:5432/codecraft-web";
+  private static final String USER = "codecraft";
+  private static final String PASSWORD = "9h2A66KSVscyh6";
 
   @Override
   public void contextInitialized(ServletContextEvent sce) {
-    String url = "jdbc:postgresql://localhost:5432/codecraft-web";
-    String user = "codecraft";
-    String password = "9h2A66KSVscyh6";
 
+    migrateCore();
+    migrateBase();
+  }
+
+  private MigrateResult migrateCore() {
     Flyway flywayCore =
         Flyway.configure()
-            .dataSource(url, user, password)
+            .dataSource(URL, USER, PASSWORD)
             .table("flyway_codecraft_core_schema_history")
             .baselineVersion("1.0.0.0001")
-            .locations("classpath:db/migration")
+            .locations("org/codecraft/apps/core/db/migration", "core/db/migration")
             .schemas("public")
             .sqlMigrationPrefix("Core_V")
             .baselineOnMigrate(true)
             .load();
 
+    return migrate(flywayCore);
+  }
+
+  private MigrateResult migrateBase() {
     Flyway flywayBase =
         Flyway.configure()
-            .dataSource(url, user, password)
+            .dataSource(URL, USER, PASSWORD)
             .table("flyway_codecraft_base_schema_history")
             .baselineVersion("1.0.0.0001")
             .locations("org/codecraft/apps/base/db/migration", "base/db/migration")
@@ -33,8 +44,10 @@ public class TestServletContextListener implements ServletContextListener {
             .sqlMigrationPrefix("Base_V")
             .baselineOnMigrate(true)
             .load();
+    return migrate(flywayBase);
+  }
 
-    flywayCore.migrate();
-    flywayBase.migrate();
+  private MigrateResult migrate(Flyway flyway) {
+    return flyway.migrate();
   }
 }
